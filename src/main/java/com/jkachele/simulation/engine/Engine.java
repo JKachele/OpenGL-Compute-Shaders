bc4098f22f3e726cfbb1aaaeea5059f52cc9ee9c
Justin Kachele <justin@kachele.com>
2022-10-18 17:34:45 -0400

    Created Window

--- src/main/java/com/jkachele/simulation/engine/Engine.java
+++ src/main/java/com/jkachele/simulation/engine/Engine.java
@@ -7,5 +7,54 @@
  ******************************************/
 package com.jkachele.simulation.engine;
 
-public class Engine {
+import com.jkachele.simulation.util.Color;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+
+public class Engine implements Runnable{
+
+    private Color backgroundColor;
+
+    private final Thread GAME_LOOP_THREAD;
+
+    public Engine(int width, int height, String title, Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+        GAME_LOOP_THREAD = new Thread(this, "GAME_LOOP_THREAD");
+        Window.init(width, height, title, backgroundColor);
+    }
+
+    public void start() {
+        GAME_LOOP_THREAD.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Window.start();
+            gameLoop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void gameLoop() {
+        // Set the clear color
+        glClearColor(backgroundColor.getRed(), backgroundColor.getGreen(),
+                backgroundColor.getBlue(), backgroundColor.getAlpha());
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( !glfwWindowShouldClose(Window.glfwWindow) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            glfwSwapBuffers(Window.glfwWindow); // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
 }
--- src/main/java/com/jkachele/simulation/engine/Window.java
+++ src/main/java/com/jkachele/simulation/engine/Window.java
@@ -7,5 +7,94 @@
  ******************************************/
 package com.jkachele.simulation.engine;
 
-public class Window {
+import com.jkachele.simulation.util.Color;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public enum Window {;
+
+    // The window handle
+    public static long glfwWindow;
+
+    private static int width;
+    private static int height;
+    private static String title;
+    private static Color backgroundColor;
+    private static boolean reset;
+
+
+    public static void init(int width, int height, String title, Color backgroundColor) {
+        Window.width = width;
+        Window.height = height;
+        Window.title = title;
+        Window.backgroundColor = backgroundColor;
+    }
+
+    public static void start() {
+        // Set up an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if ( glfwWindow == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Set up a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(glfwWindow, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(glfwWindow, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            assert vidmode != null;
+            glfwSetWindowPos(
+                    glfwWindow,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
 }
