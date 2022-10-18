f882434f19edfd3cc377ff9c5ae6a9d22f187872
Justin Kachele <justin@kachele.com>
2022-10-18 17:03:08 -0400

    initial commit

---
+++ src/main/java/com/jkachele/simulation/Main.java
@@ -0,0 +1,12 @@
+/******************************************
+ *Project-------OpenGL-Compute_Shaders
+ *File----------Main.java
+ *Author--------Justin Kachele
+ *Date----------10/18/2022
+ *License-------GNU GENERAL PUBLIC LICENSE
+ ******************************************/
+package com.jkachele.simulation;
+
+public class Main {
+
+}
---
+++ src/main/java/com/jkachele/simulation/engine/Engine.java
@@ -0,0 +1,11 @@
+/******************************************
+ *Project-------OpenGL-Compute_Shaders
+ *File----------Engine.java
+ *Author--------Justin Kachele
+ *Date----------10/18/2022
+ *License-------GNU GENERAL PUBLIC LICENSE
+ ******************************************/
+package com.jkachele.simulation.engine;
+
+public class Engine {
+}
